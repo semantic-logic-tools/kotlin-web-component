e9f9edfd893e29bbdf3e96755c81eb2b80aeb9ba
Maxime Van Assche <maxime.van.assche@logic.tools>
2022-08-02 10:40:44 +0200

    Minimize the external js code
    still trying to get rig of WebComponent declaration

--- src/jsMain/kotlin/logic/tools/web/components/WebComponent.kt
+++ src/jsMain/kotlin/logic/tools/web/components/WebComponent.kt
@@ -37,9 +37,29 @@ open class BaseWebComponent : WebComponent() {
 }
 
 
-external fun defineWebComponent(tagName: String, wc: JsClass<out WebComponent>, observedAttributes: Array<String>)
 
 fun defineWebComponent(tagName: String, wc: JsClass<out WebComponent>) {
     val observedAttributes = (wc.asDynamic().Companion?.observedAttributes?.call() as? Array<String>) ?: emptyArray<String>()
     defineWebComponent(tagName, wc, observedAttributes)
 }
+
+//external fun defineWebComponent(tagName: String, wc: JsClass<out WebComponent>, observedAttributes: Array<String>)
+private fun defineWebComponent(tagName: String, wc: JsClass<out WebComponent>, observedAttributes: Array<String>) {
+    function<(String, JsClass<out WebComponent>, Array<String>) -> () -> Unit>("tagName", "proto", "observedAttributes", block = """
+    globalThis.WebComponent = function() {}
+    let wc = class extends HTMLElement {
+        constructor() {
+            super();
+            proto.prototype.constructor.call(this);
+        }
+        static get observedAttributes() { return observedAttributes; }
+    }
+    Object.assign(wc.prototype, proto.prototype);
+    window.customElements.define(tagName, wc);
+""")(tagName, wc, observedAttributes)
+}
+
+@JsName("Function")
+private external fun <T> function(vararg params: String, block: String): T
+
+
