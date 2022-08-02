052acec0b97513a9e17d48aa830dacbb1b6e9786
Maxime Van Assche <maxime.van.assche@logic.tools>
2022-08-02 13:59:15 +0200

    Add publishing gradle
    version 1.0 published

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,3 +40,16 @@ kotlin {
 }
 
 */
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mvanassche/kotlin-web-component")
+            credentials {
+                username = project.findProperty("github.user") as? String
+                password = project.findProperty("github.key") as? String
+            }
+        }
+    }
+}
